25c1a852964a4ab97e51a0e7ef86c41c6fd76cd4
MicaiahReid <micaiahreid@gmail.com>
2024-02-10 00:08:17 -0500

    add build.rs to `npm run build` sibling `txtx-web-ui` project

---
+++ components/txtx-cli/build.rs
@@ -0,0 +1,21 @@
+use npm_rs::*;
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=../../../txtx-web-ui");
+    println!("cargo:warning=pls");
+    println!(
+        "cargo:warning={:?}",
+        std::path::Path::new("../../txtx-web-ui")
+    );
+    let exit_status = NpmEnv::default()
+        .set_path(std::path::Path::new("../../../txtx-web-ui"))
+        .with_node_env(&NodeEnv::Production)
+        .with_env("FOO", "bar")
+        .init_env()
+        .install(None)
+        .run("build")
+        .exec()
+        .unwrap();
+    println!("cargo:warning={}", exit_status);
+}
