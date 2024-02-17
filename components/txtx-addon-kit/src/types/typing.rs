419b5304e251270068857339fc0dc2fb3c97de5d
MicaiahReid <micaiahreid@gmail.com>
2024-02-16 20:49:31 -0500

    add Serialize impls for addon structs

--- components/txtx-addon-kit/src/types/typing.rs
+++ components/txtx-addon-kit/src/types/typing.rs
@@ -1,3 +1,4 @@
+use serde::{Serialize, Serializer};
 #[derive(Clone, Debug)]
 pub enum Value {
     String(String),
@@ -8,7 +9,23 @@ pub enum Value {
     Null,
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+impl Serialize for Value {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Value::String(val) => serializer.serialize_str(val),
+            Value::UnsignedInteger(val) => serializer.serialize_u64(*val),
+            Value::SignedInteger(val) => serializer.serialize_i64(*val),
+            Value::Float(val) => serializer.serialize_f64(*val),
+            Value::Bool(val) => serializer.serialize_bool(*val),
+            Value::Null => serializer.serialize_none(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize)]
 pub enum Typing {
     String,
     UnsignedInteger,
