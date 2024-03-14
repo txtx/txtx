351217fb6e027b575503c0b9b3aa1c9f6496c45a
MicaiahReid <micaiahreid@gmail.com>
2024-03-14 01:39:58 -0400

    add decode contract call command

--- components/txtx-addon-network-stacks/src/commands/mod.rs
+++ components/txtx-addon-network-stacks/src/commands/mod.rs
@@ -1,10 +1,12 @@
 use txtx_addon_kit::types::commands::CommandSpecification;
 
+mod decode_contract_call;
 mod deploy_contract;
 mod encode_contract_call;
 mod send_transaction;
 mod sign_transaction;
 
+use decode_contract_call::DECODE_STACKS_CONTRACT_CALL;
 use deploy_contract::DEPLOY_STACKS_CONTRACT;
 use encode_contract_call::ENCODE_STACKS_CONTRACT_CALL;
 use send_transaction::SEND_STACKS_TRANSACTION;
@@ -13,8 +15,9 @@ use sign_transaction::SIGN_STACKS_TRANSACTION;
 lazy_static! {
     pub static ref STACKS_COMMANDS: Vec<CommandSpecification> = vec![
         SIGN_STACKS_TRANSACTION.clone(),
+        DECODE_STACKS_CONTRACT_CALL.clone(),
         ENCODE_STACKS_CONTRACT_CALL.clone(),
         DEPLOY_STACKS_CONTRACT.clone(),
-        SEND_STACKS_TRANSACTION.clone()
+        SEND_STACKS_TRANSACTION.clone(),
     ];
 }
