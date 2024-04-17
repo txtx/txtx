af3533580293c184927989d8fbbacd3d5f29ea1b
MicaiahReid <micaiahreid@gmail.com>
2024-04-16 21:11:29 -0400

    clean up sign_transaction action

--- components/txtx-addon-network-stacks/src/commands/actions/sign_transaction.rs
+++ components/txtx-addon-network-stacks/src/commands/actions/sign_transaction.rs
@@ -18,9 +18,9 @@ use clarity_repl::{
     },
 };
 use libsecp256k1::{PublicKey, SecretKey};
-use serde_json::Value as JsonValue;
 use std::{collections::HashMap, str::FromStr};
 use tiny_hderive::bip32::ExtendedPrivKey;
+use txtx_addon_kit::types::commands::PreCommandSpecification;
 use txtx_addon_kit::types::{
     commands::{
         CommandExecutionResult, CommandImplementation, CommandInputsEvaluationResult,
@@ -33,90 +33,51 @@ use txtx_addon_kit::types::{
 use crate::typing::STACKS_SIGNED_TRANSACTION;
 
 lazy_static! {
-  pub static ref SIGN_STACKS_TRANSACTION: CommandSpecification = define_command! {
-    SignStacksTransaction => {
-        name: "Sign Stacks Transaction",
-        matcher: "sign_transaction",
-        documentation: "Sign an encoded transaction payload",
-        inputs: [
-            no_interact: {
-                documentation: "Any valid Clarity value",
-                typing: define_object_type! [
-                    nonce: {
-                        documentation: "Transaction nonce",
-                        typing: Type::uint(),
-                        optional: false,
-                        interpolable: true
-                    },
-                    transaction_payload_bytes: {
-                        documentation: "Transaction payload",
-                        typing: Type::string(),
-                        optional: false,
-                        interpolable: true
-                    },
-                    fee: {
-                        documentation: "Transaction fee",
-                        typing: Type::uint(),
-                        optional: false,
-                        interpolable: true
-                    },
-                    sender_mnemonic: {
-                        documentation: "Mnemonic",
-                        typing: Type::string(),
-                        optional: false,
-                        interpolable: true
-                    },
-                    sender_derivation_path: {
-                        documentation: "Derivation path",
-                        typing: Type::string(),
-                        optional: false,
-                        interpolable: true
-                    }
-
-                ],
-                optional: true,
+    pub static ref SIGN_STACKS_TRANSACTION: PreCommandSpecification = define_command! {
+      SignStacksTransaction => {
+          name: "Sign Stacks Transaction",
+          matcher: "sign_transaction",
+          documentation: "Sign an encoded transaction payload",
+          inputs: [
+            nonce: {
+                documentation: "Transaction nonce",
+                typing: Type::uint(),
+                optional: false,
                 interpolable: true
             },
-            cli_interact: {
-                documentation: "Any valid Clarity value",
-                typing: define_object_type! [], // todo
-                optional: true,
+            transaction_payload_bytes: {
+                documentation: "Transaction payload",
+                typing: Type::string(),
+                optional: false,
                 interpolable: true
             },
-            web_interact: {
-                documentation: "Any valid Clarity value",
-                typing: define_object_type! [
-                  transaction_payload_bytes: {
-                      documentation: "The encoded transaction bytes to be signed.",
-                      typing: Type::buffer(),
-                      optional: false,
-                      interpolable: true
-                  },
-                  signed_transaction_bytes: {
-                      documentation: "The signed transaction bytes.",
-                      typing: Type::buffer(),
-                      optional: true,
-                      interpolable: true
-                  },
-                  nonce: {
-                      documentation: "The nonce of the address signing the transaction.",
-                      typing: Type::uint(),
-                      optional: true,
-                      interpolable: true
-                  }
-                ],
-                optional: true,
+            fee: {
+                documentation: "Transaction fee",
+                typing: Type::uint(),
+                optional: false,
+                interpolable: true
+            },
+            sender_mnemonic: {
+                documentation: "Mnemonic",
+                typing: Type::string(),
+                optional: false,
+                interpolable: true
+            },
+            sender_derivation_path: {
+                documentation: "Derivation path",
+                typing: Type::string(),
+                optional: false,
                 interpolable: true
             }
-        ],
-        outputs: [
-            signed_transaction_bytes: {
-                documentation: "The signed transaction bytes.",
-                typing: Type::string()
-            }
-        ],
-    }
-  };
+          ],
+          outputs: [
+              signed_transaction_bytes: {
+                  documentation: "The signed transaction bytes.",
+                  typing: Type::string()
+              }
+          ],
+      }
+    };
 }
 
 pub struct SignStacksTransaction;
@@ -130,174 +91,81 @@ impl CommandImplementation for SignStacksTransaction {
         args: &HashMap<String, Value>,
     ) -> Result<CommandExecutionResult, Diagnostic> {
         let mut result = CommandExecutionResult::new();
-        if let Some(Value::Object(obj)) = args.get("no_interact") {
-            // Extract nonce
-            let nonce = match obj.get("nonce") {
-                Some(Ok(Value::Primitive(PrimitiveValue::UnsignedInteger(value)))) => value.clone(),
-                _ => todo!("return diagnostic"),
-            };
-            // Extract tx_fee
-            let tx_fee = match obj.get("fee") {
-                Some(Ok(Value::Primitive(PrimitiveValue::UnsignedInteger(value)))) => value.clone(),
-                _ => todo!("return diagnostic"),
-            };
-            // Extract mnemonic
-            let mnemonic = match obj.get("sender_mnemonic") {
-                Some(Ok(Value::Primitive(PrimitiveValue::String(value)))) => value.clone(),
-                _ => todo!("return diagnostic"),
-            };
-            // Extract derivation path
-            let derivation_path = match obj.get("sender_derivation_path") {
-                Some(Ok(Value::Primitive(PrimitiveValue::String(value)))) => value.clone(),
-                _ => todo!("return diagnostic"),
-            };
+        // Extract nonce
+        let nonce = match args.get("nonce") {
+            Some(Value::Primitive(PrimitiveValue::UnsignedInteger(value))) => value.clone(),
+            _ => todo!("return diagnostic"),
+        };
+        // Extract tx_fee
+        let tx_fee = match args.get("fee") {
+            Some(Value::Primitive(PrimitiveValue::UnsignedInteger(value))) => value.clone(),
+            _ => todo!("return diagnostic"),
+        };
+        // Extract mnemonic
+        let mnemonic = match args.get("sender_mnemonic") {
+            Some(Value::Primitive(PrimitiveValue::String(value))) => value.clone(),
+            _ => todo!("return diagnostic"),
+        };
+        // Extract derivation path
+        let derivation_path = match args.get("sender_derivation_path") {
+            Some(Value::Primitive(PrimitiveValue::String(value))) => value.clone(),
+            _ => todo!("return diagnostic"),
+        };
 
-            let wallet = Wallet {
-                mnemonic,
-                derivation_path,
-            };
+        let wallet = Wallet {
+            mnemonic,
+            derivation_path,
+        };
 
-            // Extract and decode transaction_payload_bytes
-            let transaction_payload_bytes = match obj.get("transaction_payload_bytes") {
-                Some(Ok(Value::Primitive(PrimitiveValue::Buffer(bytes)))) => bytes.clone(),
-                _ => todo!("transaction_payload_bytes invalid, return diagnostic"),
-            };
-            let transaction_payload = match TransactionPayload::consensus_deserialize(
-                &mut &transaction_payload_bytes.bytes[..],
-            ) {
-                Ok(res) => res,
-                Err(e) => {
-                    todo!(
-                        "transaction payload invalid, return diagnostic ({})",
-                        e.to_string()
-                    )
-                }
-            };
-            // Sign
-            let signed_transaction = match sign_transaction_payload(
-                &wallet,
-                transaction_payload,
-                nonce,
-                tx_fee,
-                TransactionAnchorMode::OffChainOnly, // todo(lgalabru)
-                &TransactionVersion::Mainnet,        // todo(lgalabru)
-            ) {
-                Ok(res) => res,
-                Err(_e) => {
-                    todo!("return diagnostic")
-                }
-            };
-
-            let mut bytes = vec![];
-            signed_transaction.consensus_serialize(&mut bytes).unwrap();
-            let value = Value::buffer(bytes, STACKS_SIGNED_TRANSACTION.clone());
-
-            result
-                .outputs
-                .insert("signed_transaction_bytes".to_string(), value);
-        } else if let Some(web_interact_inputs) = args.get("web_interact") {
-            let bytes = match web_interact_inputs {
-                Value::Object(obj) => obj.get("signed_transaction_bytes"),
-                _ => unimplemented!(),
-            };
-            match bytes {
-                Some(bytes) => match bytes {
-                    Ok(bytes) => {
-                        result
-                            .outputs
-                            .insert("signed_transaction_bytes".to_string(), bytes.clone());
-                    }
-                    Err(e) => return Err(e.clone()),
-                },
-                None => {}
+        // Extract and decode transaction_payload_bytes
+        let transaction_payload_bytes = match args.get("transaction_payload_bytes") {
+            Some(Value::Primitive(PrimitiveValue::Buffer(bytes))) => bytes.clone(),
+            _ => todo!("transaction_payload_bytes invalid, return diagnostic"),
+        };
+        let transaction_payload = match TransactionPayload::consensus_deserialize(
+            &mut &transaction_payload_bytes.bytes[..],
+        ) {
+            Ok(res) => res,
+            Err(e) => {
+                todo!(
+                    "transaction payload invalid, return diagnostic ({})",
+                    e.to_string()
+                )
             }
-        }
+        };
+        // Sign
+        let signed_transaction = match sign_transaction_payload(
+            &wallet,
+            transaction_payload,
+            nonce,
+            tx_fee,
+            TransactionAnchorMode::OffChainOnly, // todo(lgalabru)
+            &TransactionVersion::Mainnet,        // todo(lgalabru)
+        ) {
+            Ok(res) => res,
+            Err(_e) => {
+                todo!("return diagnostic")
+            }
+        };
+
+        let mut bytes = vec![];
+        signed_transaction.consensus_serialize(&mut bytes).unwrap();
+        let value = Value::buffer(bytes, STACKS_SIGNED_TRANSACTION.clone());
+
+        result
+            .outputs
+            .insert("signed_transaction_bytes".to_string(), value);
 
         Ok(result)
     }
 
     fn update_input_evaluation_results_from_user_input(
-        ctx: &CommandSpecification,
-        current_input_evaluation_result: &mut CommandInputsEvaluationResult,
-        input_name: String,
-        value: String,
+        _ctx: &CommandSpecification,
+        _current_input_evaluation_result: &mut CommandInputsEvaluationResult,
+        _input_name: String,
+        _value: String,
     ) {
-        let (input_key, value) = match input_name.as_str() {
-            "description" => {
-                let description_input =
-                    ctx.inputs.iter().find(|i| i.name == "description").expect(
-                        "Sign Stacks Transaction specification must have description input",
-                    );
-
-                let expected_type = description_input.typing.clone();
-                let value = if value.is_empty() {
-                    None
-                } else {
-                    Some(Value::from_string(value, expected_type, None))
-                };
-                (description_input, value)
-            }
-            "web_interact" => {
-                let mut object_values = HashMap::new();
-                let web_interact_input =
-                    ctx.inputs.iter().find(|i| i.name == "web_interact").expect(
-                        "Sign Stacks Transaction specification must have a web_interact input",
-                    );
-                let web_interact_input_object = web_interact_input
-                    .as_object()
-                    .expect("Sign Stacks Transaction web interact input must be and object.");
-
-                let value_json: JsonValue = match serde_json::from_str(&value) {
-                    Ok(value) => value,
-                    Err(_e) => unimplemented!(), // todo: return diagnostic
-                };
-                let value_json = value_json.as_object().unwrap(); // todo
-
-                let transaction_signature_property = web_interact_input_object.iter().find(|p| p.name == "signed_transaction_bytes").expect("Sign Stacks Transaction specification's web_interact input should have a signed_transaction_bytes property.");
-                let expected_type = transaction_signature_property.typing.clone();
-                let value = match value_json.get("signed_transaction_bytes") {
-                    Some(value) => Some(Value::from_string(
-                        value.as_str().unwrap().to_string(),
-                        expected_type,
-                        Some(STACKS_SIGNED_TRANSACTION.clone()),
-                    )),
-                    None => None,
-                };
-                match value {
-                    Some(value) => {
-                        object_values.insert(transaction_signature_property.name.clone(), value);
-                    }
-                    None => {}
-                };
-
-                let nonce_property = web_interact_input_object.iter().find(|p| p.name == "nonce").expect("Send Stacks Transaction specification's web_interact input should have a nonce property.");
-                let nonce_expected_type = nonce_property.typing.clone();
-                let nonce_val = match value_json.get("nonce") {
-                    Some(value) => Some(Value::from_string(
-                        value.to_string(),
-                        nonce_expected_type,
-                        None,
-                    )),
-                    None => None,
-                };
-                match nonce_val {
-                    Some(value) => {
-                        object_values.insert(nonce_property.name.clone(), value);
-                    }
-                    None => {}
-                };
-
-                let result = Some(Ok(Value::Object(object_values)));
-                (web_interact_input, result)
-            }
-            _ => unimplemented!("cannot parse serialized output for input {input_name}"),
-        };
-        match value {
-            Some(value) => current_input_evaluation_result
-                .inputs
-                .insert(input_key.clone(), value),
-            None => current_input_evaluation_result.inputs.remove(&input_key),
-        };
+        unimplemented!()
     }
 }
 
