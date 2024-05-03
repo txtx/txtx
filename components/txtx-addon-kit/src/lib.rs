99563664cffcce0e6b3c362a8f3e9168aa212338
Ludo Galabru <ludovic@galabru.com>
2024-05-03 15:15:32 -0500

    refactor: addon management

--- components/txtx-addon-kit/src/lib.rs
+++ components/txtx-addon-kit/src/lib.rs
@@ -1,13 +1,21 @@
 #[macro_use]
 extern crate serde_derive;
 
+use rust_fsm::StateMachine;
 pub use uuid;
 
 use hcl::structure::Block;
 pub use hcl_edit as hcl;
-use std::fmt::Debug;
+use std::{
+    collections::HashMap,
+    fmt::Debug,
+    sync::{Arc, Mutex},
+};
 use types::{
-    commands::{CommandId, CommandInstanceOrParts, PreCommandSpecification},
+    commands::{
+        CommandId, CommandInstance, CommandInstanceOrParts, CommandInstanceStateMachine,
+        CommandInstanceType, PreCommandSpecification,
+    },
     diagnostics::Diagnostic,
     functions::FunctionSpecification,
     ConstructUuid, PackageUuid,
@@ -30,23 +38,95 @@ pub trait Addon: Debug + Sync + Send {
     ///
     fn get_prompts(&self) -> Vec<PreCommandSpecification>;
     ///
-    fn create_context(self: &Self) -> Box<dyn AddonContext>;
+    fn build_function_lookup(self: &Self) -> HashMap<String, FunctionSpecification> {
+        let mut functions = HashMap::new();
+        for function in self.get_functions().into_iter() {
+            functions.insert(function.name.clone(), function);
+        }
+        functions
+    }
+    ///
+    fn build_command_lookup(self: &Self) -> HashMap<CommandId, PreCommandSpecification> {
+        let mut commands = HashMap::new();
+
+        for command in self.get_actions().into_iter() {
+            let matcher = match &command {
+                PreCommandSpecification::Atomic(command) => command.matcher.clone(),
+                PreCommandSpecification::Composite(command) => command.matcher.clone(),
+            };
+            commands.insert(CommandId::Action(matcher), command);
+        }
+
+        for command in self.get_prompts().into_iter() {
+            let matcher = match &command {
+                PreCommandSpecification::Atomic(command) => command.matcher.clone(),
+                PreCommandSpecification::Composite(command) => command.matcher.clone(),
+            };
+            commands.insert(CommandId::Prompt(matcher), command);
+        }
+        commands
+    }
+
+    fn create_context(&self) -> AddonContext {
+        AddonContext {
+            functions: self.build_function_lookup(),
+            commands: self.build_command_lookup(),
+        }
+    }
 }
 
-///
-pub trait AddonContext: Debug + Sync + Send {
-    ///
-    fn create_command_instance(
+#[derive(Debug)]
+pub struct AddonContext {
+    pub functions: HashMap<String, FunctionSpecification>,
+    pub commands: HashMap<CommandId, PreCommandSpecification>,
+}
+
+impl AddonContext {
+    pub fn create_command_instance(
         self: &Self,
         command_id: &CommandId,
         namespace: &str,
         command_name: &str,
         block: &Block,
         package_uuid: &PackageUuid,
-    ) -> Result<CommandInstanceOrParts, Diagnostic>;
-    ///
-    fn resolve_construct_dependencies(
+    ) -> Result<CommandInstanceOrParts, Diagnostic> {
+        let Some(pre_command_spec) = self.commands.get(command_id) else {
+            todo!("return diagnostic: unknown command: {:?}", command_id)
+        };
+        let typing = match command_id {
+            CommandId::Action(_) => CommandInstanceType::Action,
+            CommandId::Prompt(_) => CommandInstanceType::Prompt,
+        };
+        match pre_command_spec {
+            PreCommandSpecification::Atomic(command_spec) => {
+                let command_instance = CommandInstance {
+                    specification: command_spec.clone(),
+                    state: Arc::new(Mutex::new(
+                        StateMachine::<CommandInstanceStateMachine>::new(),
+                    )),
+                    name: command_name.to_string(),
+                    block: block.clone(),
+                    package_uuid: package_uuid.clone(),
+                    typing,
+                    namespace: Some(namespace.to_string()),
+                };
+                Ok(CommandInstanceOrParts::Instance(command_instance))
+            }
+            PreCommandSpecification::Composite(composite_spec) => {
+                let bodies = (composite_spec.router)(
+                    &block.body.to_string(),
+                    &command_name.to_string(),
+                    &composite_spec.parts,
+                )?;
+                Ok(CommandInstanceOrParts::Parts(bodies))
+            }
+        }
+    }
+
+    pub fn resolve_construct_dependencies(
         self: &Self,
-        construct_uuid: &ConstructUuid,
-    ) -> Vec<ConstructUuid>;
+        _construct_uuid: &ConstructUuid,
+    ) -> Vec<ConstructUuid> {
+        vec![]
+    }
 }
--- components/txtx-core/src/std/mod.rs
+++ components/txtx-core/src/std/mod.rs
@@ -1,2 +1,39 @@
+use txtx_addon_kit::{
+    types::{commands::PreCommandSpecification, functions::FunctionSpecification},
+    Addon,
+};
+
 pub mod commands;
 pub mod functions;
+
+lazy_static! {
+    pub static ref ACTIONS: Vec<PreCommandSpecification> =
+        vec![commands::http::SEND_HTTP_REQUEST.clone(),];
+}
+
+#[derive(Debug)]
+pub struct StdAddon;
+
+impl StdAddon {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Addon for StdAddon {
+    fn get_namespace(&self) -> &str {
+        "std"
+    }
+
+    fn get_functions(&self) -> Vec<FunctionSpecification> {
+        vec![]
+    }
+
+    fn get_actions(&self) -> Vec<PreCommandSpecification> {
+        ACTIONS.clone()
+    }
+
+    fn get_prompts(&self) -> Vec<PreCommandSpecification> {
+        vec![]
+    }
+}
