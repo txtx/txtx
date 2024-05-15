7d91c1735d6a6c72765107e57ebbe3634b4c7f18
MicaiahReid <micaiahreid@gmail.com>
2024-05-15 01:38:03 -0400

    document set_default_network

--- components/txtx-addon-network-stacks/src/commands/actions/set_default_network.rs
+++ components/txtx-addon-network-stacks/src/commands/actions/set_default_network.rs
@@ -13,16 +13,23 @@ lazy_static! {
             SetStacksGlobals => {
                 name: "Configure Stacks Network",
                 matcher: "set_default_network",
-                documentation: "Configure Stacks Network.",
+                documentation: indoc!{r#"
+                  The `set_default_network` action can be used to set default network data for Stacks Runbook inputs.
+                  Any commands that have an input that matches the name of one of these default inputs will automatically have these default inputs applied.
+                  This allows for more terse Runbooks, as some redundant data can be omitted.
+
+                  For example, the `network_id` input is used in many Stacks txtx commands. 
+                  By setting this input once with `set_default_network`, the `network_id` can be omitted from subsequent Stacks txtx commands.
+                "#},
                 inputs: [
                     network_id: {
-                        documentation: "Network to use (mainnet, testnet).",
+                        documentation: indoc!{r#"A default Stacks network id to use. Valid values are `"mainnet"` and `"testnet"`."#},
                         typing: Type::string(),
                         optional: false,
                         interpolable: true
                     },
                     stacks_api_url: {
-                        documentation: "Stacks API RPC URL.",
+                        documentation: "A default Stacks API RPC URL to use.",
                         typing: Type::string(),
                         optional: false,
                         interpolable: true
@@ -31,18 +38,18 @@ lazy_static! {
                 outputs: [],
                 example: txtx_addon_kit::indoc! {r#"
                 action "my_ref" "stacks::set_default_network" {
-                    description = "Encodes the contract call, prompts the user to sign, and broadcasts the set-token function."
-                    contract_id = "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.pyth-oracle-v1"
-                    function_name = "verify-and-update-price-feeds"
-                    function_args = [
-                        encode_buffer(output.bitcoin_price_feed),
-                        encode_tuple({
-                            "pyth-storage-contract": encode_principal("${env.pyth_deployer}.pyth-store-v1"),
-                            "pyth-decoder-contract": encode_principal("${env.pyth_deployer}.pyth-pnau-decoder-v1"),
-                            "wormhole-core-contract": encode_principal("${env.pyth_deployer}.wormhole-core-v1")
-                        })
-                    ]
+                    description = "Sets the default network id and Stacks API url."
+                    network_id = "mainnet"
+                    stacks_api_url = "https://api.mainnet.hiro.so"
                 }
+                prompt "signed_bytes" "stacks::sign_transaction" {
+                  transaction_payload_bytes = encode_buffer("0x021A6D78DE7B0625DFBFC16C3A8A5735F6DC3DC3F2CE0E707974682D6F7261636C652D76311D7665726966792D616E642D7570646174652D70726963652D66656564730000000202000000030102030C0000000315707974682D6465636F6465722D636F6E7472616374061A6D78DE7B0625DFBFC16C3A8A5735F6DC3DC3F2CE14707974682D706E61752D6465636F6465722D763115707974682D73746F726167652D636F6E7472616374061A6D78DE7B0625DFBFC16C3A8A5735F6DC3DC3F2CE0D707974682D73746F72652D763116776F726D686F6C652D636F72652D636F6E7472616374061A6D78DE7B0625DFBFC16C3A8A5735F6DC3DC3F2CE10776F726D686F6C652D636F72652D7631")
+                  // network_id = "testnet" // note, the network_id can be omitted
+                }
+                output "signed_bytes" {
+                  value = prompt.my_ref.signed_transaction_bytes
+                }
+                // > signed_bytes: 0x8080000000040063A5EDA39412C016478AE5A8C300843879F78245000000000000000100000000000004B0000182C1712C31B7F683F6C56EEE8920892F735FC0118C98FD10C1FDAA85ABEC2808063773E5F61229D76B29784B8BBBBAAEA72EEA701C92A4FE15EF3B9E32A373D8020100000000021A6D78DE7B0625DFBFC16C3A8A5735F6DC3DC3F2CE0E707974682D6F7261636C652D76311D7665726966792D616E642D7570646174652D70726963652D66656564730000000202000000030102030C0000000315707974682D6465636F6465722D636F6E7472616374061A6D78DE7B0625DFBFC16C3A8A5735F6DC3DC3F2CE14707974682D706E61752D6465636F6465722D763115707974682D73746F726167652D636F6E7472616374061A6D78DE7B0625DFBFC16C3A8A5735F6DC3DC3F2CE0D707974682D73746F72652D763116776F726D686F6C652D636F72652D636F6E7472616374061A6D78DE7B0625DFBFC16C3A8A5735F6DC3DC3F2CE10776F726D686F6C652D636F72652D7631
             "#},
                 }
         };
