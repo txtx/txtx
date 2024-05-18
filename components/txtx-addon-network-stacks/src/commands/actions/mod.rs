d06a0e870c511c199b27b2490bfd551238efffee
MicaiahReid <micaiahreid@gmail.com>
2024-05-18 00:55:58 -0400

    add new action/prompt to mod

--- components/txtx-addon-network-stacks/src/commands/actions/mod.rs
+++ components/txtx-addon-network-stacks/src/commands/actions/mod.rs
@@ -2,6 +2,7 @@ pub mod broadcast_transaction;
 mod decode_contract_call;
 mod deploy_contract;
 pub mod encode_contract_call;
+mod encode_multisig;
 pub mod set_default_network;
 pub mod sign_transaction;
 
@@ -9,6 +10,7 @@ use broadcast_transaction::BROADCAST_STACKS_TRANSACTION;
 use decode_contract_call::DECODE_STACKS_CONTRACT_CALL;
 use deploy_contract::DEPLOY_STACKS_CONTRACT;
 use encode_contract_call::ENCODE_STACKS_CONTRACT_CALL;
+use encode_multisig::ENCODE_MULTISIG_TRANSACTION;
 use set_default_network::SET_DEFAULT_NETWORK;
 use sign_transaction::SIGN_STACKS_TRANSACTION;
 use txtx_addon_kit::types::commands::PreCommandSpecification;
@@ -20,6 +22,7 @@ lazy_static! {
         ENCODE_STACKS_CONTRACT_CALL.clone(),
         DEPLOY_STACKS_CONTRACT.clone(),
         BROADCAST_STACKS_TRANSACTION.clone(),
-        SET_DEFAULT_NETWORK.clone()
+        SET_DEFAULT_NETWORK.clone(),
+        ENCODE_MULTISIG_TRANSACTION.clone()
     ];
 }
