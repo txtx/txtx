b3a0017a7229fa98a7c93f2398b5f4bbd6f8e9b8
MicaiahReid <micaiahreid@gmail.com>
2024-05-27 20:42:35 -0400

    cleanup runbook gql query

--- components/txtx-gql/src/types/runbook.rs
+++ components/txtx-gql/src/types/runbook.rs
@@ -1,13 +1,7 @@
-use serde::de::value::Error;
-
 use crate::Context;
 use juniper_codegen::graphql_object;
-use serde::{de::IntoDeserializer, Deserialize};
 use serde_json::json;
-use txtx_core::{
-    eval::{get_sorted_nodes, is_child_of_node},
-    types::{ConstructUuid, Runbook},
-};
+use txtx_core::types::ConstructUuid;
 
 #[derive(Clone)]
 pub struct ProtocolManifest {
@@ -67,100 +61,5 @@ impl RunbookDescription {
     pub fn construct_uuids(&self) -> String {
         let json = json!(self.construct_uuids);
         json.to_string()
-    }
-}
-
-pub struct GqlRunbook {
-    pub name: String,
-    pub data: Runbook,
-}
-
-impl GqlRunbook {
-    pub fn new(name: String, data: Runbook) -> GqlRunbook {
-        Self {
-            name: name.clone(),
-            data: data.clone(),
-        }
-    }
-}
-
-#[graphql_object(context = Context)]
-impl GqlRunbook {
-    pub fn name(&self) -> String {
-        self.name.clone()
-    }
-
-    pub fn description(&self) -> Option<String> {
-        self.data.description.clone()
-    }
-
-    pub fn uuid(&self) -> String {
-        self.name.clone()
-    }
-
-    pub fn data(&self) -> Result<String, String> {
-        let mut data = vec![];
-        let ordered_nodes = get_sorted_nodes(self.data.constructs_graph.clone());
-        let graph = self.data.constructs_graph.clone();
-
-        for (i, node) in ordered_nodes.into_iter().enumerate() {
-            let uuid = graph
-                .node_weight(node)
-                .expect("unable to retrieve construct");
-            let construct_uuid = ConstructUuid::Local(uuid.clone());
-
-            let Some(command_instance) = self.data.commands_instances.get(&construct_uuid) else {
-                continue;
-            };
-
-            let is_child_of_root = is_child_of_node(self.data.graph_root, node, &graph);
-
-            let constructs_execution_results =
-                match self.data.constructs_execution_results.get(&construct_uuid) {
-                    None => None,
-                    Some(result) => match result {
-                        Ok(result) => Some(
-                            serde_json::to_value(result)
-                                .map_err(|e| format!("failed to serialize runbook data {e}"))?,
-                        ),
-                        Err(e) => Some(json!({"error": e})),
-                    },
-                };
-            let command_inputs_evaluation_results = self
-                .data
-                .command_inputs_evaluation_results
-                .get(&construct_uuid);
-
-            data.push(json!({
-                "readonly": !is_child_of_root,
-                "index": i,
-                "constructUuid": construct_uuid,
-                "commandInstance": command_instance,
-                "commandInputsEvaluationResult": command_inputs_evaluation_results,
-                "constructsExecutionResult": constructs_execution_results
-            }));
-        }
-
-        serde_json::to_string(&data).map_err(|e| format!("failed to serialize runbook data {e}"))
-    }
-
-    pub fn command_instance_state(&self, construct_uuid_string: String) -> Result<String, String> {
-        let construct_uuid =
-            ConstructUuid::deserialize(construct_uuid_string.clone().into_deserializer())
-                .map_err(|e: Error| e.to_string())?;
-
-        let result = match self.data.commands_instances.get(&construct_uuid) {
-            Some(command_instance) => {
-                json!({"state": command_instance.state_machine.state() })
-            }
-            None => json!({}),
-        };
-
-        serde_json::to_string(&result).map_err(|e| {
-            format!(
-                "failed to serialize command instance {} state {}",
-                construct_uuid_string, e
-            )
-        })
     }
 }
