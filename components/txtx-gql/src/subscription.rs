4b1672f59b198cab32fb9cd03faa0f3160e9dfcd
MicaiahReid <micaiahreid@gmail.com>
2024-06-01 00:21:24 -0400

    split subscription queries by block event

--- components/txtx-gql/src/subscription.rs
+++ components/txtx-gql/src/subscription.rs
@@ -1,23 +1,70 @@
 use std::pin::Pin;
 
-use crate::{types::block::GqlBlockEvent, Context};
+use crate::{
+    types::block::{GqlBlock, GqlSetActionItemStatus},
+    Context,
+};
 use futures::Stream;
 use juniper::{graphql_subscription, FieldError};
+use txtx_core::kit::types::frontend::BlockEvent;
 
 pub struct Subscription;
 
-type GqlBlockStream = Pin<Box<dyn Stream<Item = Result<GqlBlockEvent, FieldError>> + Send>>;
+type GqlBlockStream = Pin<Box<dyn Stream<Item = Result<GqlBlock, FieldError>> + Send>>;
+
+type GqlSetActionItemStatusStream =
+    Pin<Box<dyn Stream<Item = Result<Vec<GqlSetActionItemStatus>, FieldError>> + Send>>;
+
+type ClearBlockEventStream = Pin<Box<dyn Stream<Item = Result<bool, FieldError>> + Send>>;
+
 #[graphql_subscription(
   context = Context,
 )]
 impl Subscription {
-    async fn blocks(context: &Context) -> GqlBlockStream {
+    async fn append_block_event(context: &Context) -> GqlBlockStream {
         let block_tx = context.block_broadcaster.clone();
         let mut block_rx = block_tx.subscribe();
         let stream = async_stream::stream! {
             loop {
               if let Ok(block_event) = block_rx.recv().await {
-                yield Ok(GqlBlockEvent::new(block_event));
+                match block_event {
+                    BlockEvent::Append(block) => yield Ok(GqlBlock::new(block)),
+                    _ => {}
+                }
+
+                }
+              }
+
+        };
+        Box::pin(stream)
+    }
+
+    async fn update_action_items_event(context: &Context) -> GqlSetActionItemStatusStream {
+        let block_tx = context.block_broadcaster.clone();
+        let mut block_rx = block_tx.subscribe();
+        let stream = async_stream::stream! {
+            loop {
+              if let Ok(block_event) = block_rx.recv().await {
+                match block_event {
+                    BlockEvent::UpdateActionItems(updates) => yield Ok(updates.into_iter().map(GqlSetActionItemStatus::new).collect()),
+                    _ => {}
+                }
+              }
+            }
+        };
+        Box::pin(stream)
+    }
+
+    async fn clear_blocks_event(context: &Context) -> ClearBlockEventStream {
+        let block_tx = context.block_broadcaster.clone();
+        let mut block_rx = block_tx.subscribe();
+        let stream = async_stream::stream! {
+            loop {
+              if let Ok(block_event) = block_rx.recv().await {
+                match block_event {
+                  BlockEvent::Clear => yield Ok(true),
+                    _ => {}
+                }
               }
             }
         };
