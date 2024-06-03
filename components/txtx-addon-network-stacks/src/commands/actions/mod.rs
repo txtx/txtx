4d71372af06fa1c51cad86defc01b5cb939b0925
MicaiahReid <micaiahreid@gmail.com>
2024-06-02 23:49:25 -0400

    start fixing send_contract_call

--- components/txtx-addon-network-stacks/src/commands/actions/mod.rs
+++ components/txtx-addon-network-stacks/src/commands/actions/mod.rs
@@ -4,6 +4,7 @@ mod decode_contract_call;
 mod deploy_contract;
 pub mod encode_contract_call;
 mod encode_multisig;
+mod send_contract_call;
 pub mod set_default_network;
 pub mod sign_transaction;
 
@@ -13,6 +14,7 @@ use decode_contract_call::DECODE_STACKS_CONTRACT_CALL;
 use deploy_contract::DEPLOY_STACKS_CONTRACT;
 use encode_contract_call::ENCODE_STACKS_CONTRACT_CALL;
 use encode_multisig::ENCODE_MULTISIG_TRANSACTION;
+use send_contract_call::SEND_CONTRACT_CALL;
 use set_default_network::SET_DEFAULT_NETWORK;
 use sign_transaction::SIGN_STACKS_TRANSACTION;
 use txtx_addon_kit::types::commands::PreCommandSpecification;
@@ -26,6 +28,7 @@ lazy_static! {
         BROADCAST_STACKS_TRANSACTION.clone(),
         CALL_READONLY_FN.clone(),
         SET_DEFAULT_NETWORK.clone(),
-        ENCODE_MULTISIG_TRANSACTION.clone()
+        ENCODE_MULTISIG_TRANSACTION.clone(),
+        SEND_CONTRACT_CALL.clone(),
     ];
 }
