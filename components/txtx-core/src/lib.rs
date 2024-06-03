eb729afcb758b3dba22d3bc3f58258f4e728e35a
MicaiahReid <micaiahreid@gmail.com>
2024-06-02 20:32:33 -0400

    ValidatePanel button status default to ToDo

--- components/txtx-core/src/lib.rs
+++ components/txtx-core/src/lib.rs
@@ -257,7 +257,7 @@ pub async fn start_runbook_runloop(
                         index: 0,
                         title: "Validate".into(),
                         description: "".into(),
-                        action_status: ActionItemStatus::Success(None),
+                        action_status: ActionItemStatus::Todo,
                         action_type: ActionItemRequestType::ValidatePanel,
                     }]));
                 }
