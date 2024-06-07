5a0e030b8f2d9fe6b9ce3a5520bb85887af48abc
MicaiahReid <micaiahreid@gmail.com>
2024-06-07 01:14:00 -0400

    remove artifacts of deleted file

--- components/txtx-addon-network-stacks/src/commands/actions/mod.rs
+++ components/txtx-addon-network-stacks/src/commands/actions/mod.rs
@@ -3,7 +3,6 @@ pub mod call_readonly_fn;
 mod decode_contract_call;
 mod deploy_contract;
 pub mod encode_contract_call;
-mod encode_multisig;
 mod send_contract_call;
 pub mod set_default_network;
 pub mod sign_transaction;
@@ -13,7 +12,6 @@ use call_readonly_fn::CALL_READONLY_FN;
 use decode_contract_call::DECODE_STACKS_CONTRACT_CALL;
 use deploy_contract::DEPLOY_STACKS_CONTRACT;
 use encode_contract_call::ENCODE_STACKS_CONTRACT_CALL;
-use encode_multisig::ENCODE_MULTISIG_TRANSACTION;
 use send_contract_call::SEND_CONTRACT_CALL;
 use set_default_network::SET_DEFAULT_NETWORK;
 use sign_transaction::SIGN_STACKS_TRANSACTION;
@@ -28,7 +26,6 @@ lazy_static! {
         BROADCAST_STACKS_TRANSACTION.clone(),
         CALL_READONLY_FN.clone(),
         SET_DEFAULT_NETWORK.clone(),
-        ENCODE_MULTISIG_TRANSACTION.clone(),
         SEND_CONTRACT_CALL.clone(),
     ];
 }
