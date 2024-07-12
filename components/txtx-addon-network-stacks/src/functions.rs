55e6e48ad0e10a5a19b1e1cc53e1de248760b7fa
Micaiah Reid <micaiahreid@gmail.com>
2024-07-11 21:29:47 -0400

    chore(stacks) add `cv_some` & `cv_none` (#110)
    * chore(stacks): add `cv_some`, `cv_none`
    
    * chore: fix broadcast complete grammar

--- components/txtx-addon-network-stacks/src/functions.rs
+++ components/txtx-addon-network-stacks/src/functions.rs
@@ -1,5 +1,6 @@
 use clarity::vm::types::{
-    ASCIIData, BuffData, CharType, PrincipalData, SequenceData, SequencedValue, UTF8Data,
+    ASCIIData, BuffData, CharType, OptionalData, PrincipalData, SequenceData, SequencedValue,
+    UTF8Data,
 };
 use clarity_repl::clarity::{codec::StacksMessageCodec, Value as ClarityValue};
 use txtx_addon_kit::types::{
@@ -18,6 +19,45 @@ use crate::{
 
 lazy_static! {
     pub static ref FUNCTIONS: Vec<FunctionSpecification> = vec![
+        define_function! {
+            EncodeClarityValueSome => {
+                name: "cv_some",
+                documentation: "`stacks::cv_some` wraps the given Clarity value in a Clarity `Optional`.",
+                example: indoc! {r#"
+                output "some" { 
+                  value = stacks::cv_some(stacks::cv_bool(true))
+                }
+                // > some: 0x0a03
+                "#},
+                inputs: [
+                    clarity_value: {
+                        documentation: "A Clarity Value.",
+                        typing: vec![Type::uint()]
+                    }
+                ],
+                output: {
+                    documentation: "The input Clarity value wrapped in a Clarity `Optional`.",
+                    typing: Type::uint()
+                },
+            }
+        },
+        define_function! {
+            EncodeClarityValueNone => {
+                name: "cv_none",
+                documentation: "`stacks::cv_none` returns the Clarity value `None`.",
+                example: indoc! {r#"
+                output "none" { 
+                  value = stacks::cv_none()
+                }
+                // > none: 0x09
+                "#},
+                inputs: [],
+                output: {
+                    documentation: "The Clarity value `None`.",
+                    typing: Type::uint()
+                },
+            }
+        },
         define_function! {
             EncodeClarityValueBool => {
                 name: "cv_bool",
@@ -283,8 +323,8 @@ impl FunctionImplementation for EncodeClarityValueErr {
 }
 
 #[derive(Clone)]
-pub struct StacksEncodeSome;
-impl FunctionImplementation for StacksEncodeSome {
+pub struct EncodeClarityValueSome;
+impl FunctionImplementation for EncodeClarityValueSome {
     fn check_instantiability(
         _ctx: &FunctionSpecification,
         _args: &Vec<Type>,
@@ -292,13 +332,35 @@ impl FunctionImplementation for StacksEncodeSome {
         unimplemented!()
     }
 
-    fn run(_ctx: &FunctionSpecification, _args: &Vec<Value>) -> Result<Value, Diagnostic> {
-        unimplemented!()
+    fn run(_ctx: &FunctionSpecification, args: &Vec<Value>) -> Result<Value, Diagnostic> {
+        let entry = match args.get(0) {
+            Some(Value::Primitive(PrimitiveValue::Buffer(val))) => val.clone(),
+            Some(any) => {
+                return Err(diagnosed_error!(
+                    "'cv_some' function: expected cv, got {:?}",
+                    any
+                ))
+            }
+            None => {
+                return Err(diagnosed_error!(
+                    "'cv_some' function: expected cv, got none :("
+                ))
+            }
+        };
+        let cv = match parse_clarity_value(&entry.bytes, &entry.typing) {
+            Ok(v) => v,
+            Err(e) => return Err(e),
+        };
+        let clarity_value = ClarityValue::Optional(OptionalData {
+            data: Some(Box::new(cv)),
+        });
+        let bytes = clarity_value.serialize_to_vec();
+        Ok(Value::buffer(bytes, CLARITY_UINT.clone()))
     }
 }
 
-pub struct StacksEncodeNone;
-impl FunctionImplementation for StacksEncodeNone {
+pub struct EncodeClarityValueNone;
+impl FunctionImplementation for EncodeClarityValueNone {
     fn check_instantiability(
         _ctx: &FunctionSpecification,
         _args: &Vec<Type>,
@@ -306,8 +368,15 @@ impl FunctionImplementation for StacksEncodeNone {
         unimplemented!()
     }
 
-    fn run(_ctx: &FunctionSpecification, _args: &Vec<Value>) -> Result<Value, Diagnostic> {
-        unimplemented!()
+    fn run(_ctx: &FunctionSpecification, args: &Vec<Value>) -> Result<Value, Diagnostic> {
+        if !args.is_empty() {
+            return Err(diagnosed_error!(
+                "`cv_none` function: expected no arguments"
+            ));
+        }
+        let clarity_value = ClarityValue::Optional(OptionalData { data: None });
+        let bytes = clarity_value.serialize_to_vec();
+        Ok(Value::buffer(bytes, CLARITY_UINT.clone()))
     }
 }
 
@@ -331,7 +400,7 @@ impl FunctionImplementation for EncodeClarityValueBool {
             }
             None => {
                 return Err(diagnosed_error!(
-                    "'cv_bool' function: expected uint, got none :("
+                    "'cv_bool' function: expected bool, got none :("
                 ))
             }
         };
