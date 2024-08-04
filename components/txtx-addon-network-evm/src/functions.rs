88ff46fd6a9ba6b4ad4a231a603f8919aa5fc056
MicaiahReid <micaiahreid@gmail.com>
2024-08-04 12:51:23 -0400

    feat(evm): add `get_contract_from_foundry_project` function

--- components/txtx-addon-network-evm/src/functions.rs
+++ components/txtx-addon-network-evm/src/functions.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use alloy::{
     hex::FromHex,
     primitives::{Address, Bytes, B256},
@@ -13,6 +15,10 @@ use txtx_addon_kit::{
     },
 };
 
+use crate::{
+    codec::foundry::FoundryConfig,
+    typing::{BYTES, BYTES32, CHAIN_DEFAULTS, DEPLOYMENT_ARTIFACTS_TYPE, ETH_ADDRESS},
+};
 const INFURA_API_KEY: &str = dotenv!("INFURA_API_KEY");
 
 lazy_static! {
@@ -102,6 +108,24 @@ lazy_static! {
                 },
             }
         },
+        define_function! {
+            GetFoundryDeploymentArtifacts => {
+                name: "get_contract_from_foundry_project",
+                documentation: "Coming soon",
+                example: indoc! {r#"
+                        // Coming Soon
+                        "#},
+                inputs: [
+                    input: {
+                        documentation: "Coming Soon",
+                        typing: vec![Type::string()]
+                    }
+                ],
+                output: {
+                    documentation: "Coming Soon",
+                    typing: DEPLOYMENT_ARTIFACTS_TYPE.clone()
+                },
+            }
         }
     ];
 }
@@ -263,4 +287,94 @@ impl FunctionImplementation for EncodeEVMChain {
         );
         Ok(Value::object(obj_props))
     }
+}
+
+#[derive(Clone)]
+pub struct GetFoundryDeploymentArtifacts;
+impl FunctionImplementation for GetFoundryDeploymentArtifacts {
+    fn check_instantiability(
+        _fn_spec: &FunctionSpecification,
+        _auth_ctx: &AuthorizationContext,
+        _args: &Vec<Type>,
+    ) -> Result<Type, Diagnostic> {
+        unimplemented!()
+    }
+
+    fn run(
+        _fn_spec: &FunctionSpecification,
+        _auth_ctx: &AuthorizationContext,
+        args: &Vec<Value>,
+    ) -> Result<Value, Diagnostic> {
+        let prefix = "command 'evm::get_contract_from_foundry_project'";
+        let manifest_file_path = match args.get(0) {
+            Some(Value::Primitive(PrimitiveValue::String(val))) => val.clone(),
+            other => return Err(format_fn_error(&prefix, 1, "string", other)),
+        };
+        let contract_filename = match args.get(1) {
+            Some(Value::Primitive(PrimitiveValue::String(val))) => val.clone(),
+            other => return Err(format_fn_error(&prefix, 2, "string", other)),
+        };
+
+        let contract_name = match args.get(2) {
+            Some(Value::Primitive(PrimitiveValue::String(val))) => val.clone(),
+            None => contract_filename.clone(),
+            other => return Err(format_fn_error(&prefix, 3, "string", other)),
+        };
+
+        let foundry_profile = match args.get(3) {
+            Some(Value::Primitive(PrimitiveValue::String(val))) => val.clone(),
+            None => "default".into(),
+            other => return Err(format_fn_error(&prefix, 4, "string", other)),
+        };
+
+        let foundry_config = FoundryConfig::get_from_path(&manifest_file_path)
+            .map_err(|e| diagnosed_error!("{}: {}", prefix, e))?;
+
+        let compiled_output = foundry_config
+            .get_compiled_output(&contract_filename, &contract_name, Some(&foundry_profile))
+            .map_err(|e| diagnosed_error!("{}: {}", prefix, e))?;
+
+        let abi_string = serde_json::to_string(&compiled_output.abi)
+            .map_err(|e| diagnosed_error!("{}: failed to serialize abi: {}", prefix, e))?;
+
+        let init_code = compiled_output.bytecode.object.clone();
+
+        let source = compiled_output
+            .get_contract_source(&manifest_file_path, &contract_filename)
+            .map_err(|e| diagnosed_error!("{}: {}", prefix, e))?;
+
+        let abi = Value::string(abi_string);
+        let bytecode = Value::string(compiled_output.bytecode.object.clone());
+        let init_code = Value::string(init_code);
+        let source = Value::string(source);
+        let compiler_version =
+            Value::string(format!("v{}", compiled_output.metadata.compiler.version));
+        let contract_name = Value::string(contract_name.to_string());
+        let optimizer_enabled = Value::bool(compiled_output.metadata.settings.optimizer.enabled);
+        let optimizer_runs = Value::uint(compiled_output.metadata.settings.optimizer.runs);
+        let evm_version = Value::string(compiled_output.metadata.settings.evm_version);
+
+        let obj_props = indexmap::indexmap! {
+            "abi".to_string() => abi,
+            "bytecode".to_string() => bytecode,
+            "init_code".to_string() => init_code,
+            "source".to_string() => source,
+            "compiler_version".to_string() => compiler_version,
+            "contract_name".to_string() => contract_name,
+            "optimizer_enabled".to_string() => optimizer_enabled,
+            "optimizer_runs".to_string() => optimizer_runs,
+            "evm_version".to_string() => evm_version,
+        };
+        Ok(Value::object(obj_props))
+    }
+}
+
+fn format_fn_error(ctx: &str, position: u64, expected: &str, actual: Option<&Value>) -> Diagnostic {
+    return diagnosed_error!(
+        "'{}', argument position {:?}: expected {}, got {:?}",
+        ctx,
+        position,
+        expected,
+        actual.and_then(|v| Some(v.get_type()))
+    );
 }
