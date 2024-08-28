1cb9402336e7f62c4af58b29e92bd45c2439896f
Ludo Galabru <ludo@txtx.builders>
2024-08-28 13:06:35 -0400

    fix: copy pasta

--- addons/stacks/src/signers/mod.rs
+++ addons/stacks/src/signers/mod.rs
@@ -50,7 +50,7 @@ pub async fn get_addition_actions_for_address(
     if do_request_public_key {
         action_items.push(ActionItemRequest::new(
             &Some(signer_did.clone()),
-            &format!("Connect signer {instance_name}"),
+            &format!("Connect wallet {instance_name}"),
             None,
             ActionItemStatus::Todo,
             ActionItemRequestType::ProvidePublicKey(ProvidePublicKeyRequest {
