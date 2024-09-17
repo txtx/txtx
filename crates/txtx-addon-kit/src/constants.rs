f528fee08ffa036cc960b68f90709651eb68142e
MicaiahReid <micaiahreid@gmail.com>
2024-09-17 09:45:33 -0400

    chore(kit): revert change to constant

--- crates/txtx-addon-kit/src/constants.rs
+++ crates/txtx-addon-kit/src/constants.rs
@@ -1,6 +1,6 @@
 // Signers
 pub const SIGNED_MESSAGE_BYTES: &str = "signed_message_bytes";
-pub const SIGNED_TRANSACTION_BYTES: &str = "signed_transaction_result";
+pub const SIGNED_TRANSACTION_BYTES: &str = "signed_transaction_bytes";
 pub const TX_HASH: &str = "tx_hash";
 pub const SIGNATURE_APPROVED: &str = "signature_approved";
 pub const SIGNATURE_SKIPPABLE: &str = "signature_skippable";
