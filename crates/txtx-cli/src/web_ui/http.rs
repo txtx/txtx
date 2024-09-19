a26cc9c9a85c1e85237eec2777dda33f53bc7940
MicaiahReid <micaiahreid@gmail.com>
2024-09-19 13:15:59 -0400

    feat(cli/gql): expose registered addons to frontend

--- crates/txtx-cli/src/web_ui/http.rs
+++ crates/txtx-cli/src/web_ui/http.rs
@@ -139,6 +139,7 @@ async fn subscriptions(
     let ctx = GraphContext {
         protocol_name: context.protocol_name.clone(),
         runbook_name: context.runbook_name.clone(),
+        registered_addons: context.registered_addons.clone(),
         runbook_description: context.runbook_description.clone(),
         block_store: context.block_store.clone(),
         block_broadcaster: context.block_broadcaster.clone(),
