5fd2225e7c41ce757dddd410819ec0a56019cb52
MicaiahReid <micaiahreid@gmail.com>
2024-12-05 13:05:36 -0500

    feat(txtx-cli): update build script to build EVM contracts using Forge

--- crates/txtx-cli/build.rs
+++ crates/txtx-cli/build.rs
@@ -15,4 +15,19 @@ fn main() {
             .unwrap();
         println!("cargo:info={}", exit_status);
     }
+    // evm contract builds
+    {
+        use std::process::Command;
+        let contracts_dir = "../../addons/evm/src/contracts";
+
+        println!("cargo:rerun-if-changed=build.rs");
+        println!("cargo:rerun-if-changed={}", contracts_dir);
+
+        let exit_status = Command::new("forge")
+            .args(&["build"])
+            .current_dir(contracts_dir)
+            .status()
+            .expect("Failed to run `forge build`. Ensure Foundry is installed.");
+        println!("cargo:info={}", exit_status);
+    }
 }
