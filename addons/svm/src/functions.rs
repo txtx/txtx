d79c89e167a44dd18826aa3b6e832b3b35ea161a
MicaiahReid <micaiahreid@gmail.com>
2025-01-07 20:43:45 -0500

    fix(svm): fix output typing for `lamports_to_sol` fn

--- addons/svm/src/functions.rs
+++ addons/svm/src/functions.rs
@@ -218,7 +218,7 @@ lazy_static! {
                 ],
                 output: {
                     documentation: "The number of lamports provided, represented as SOL.",
-                    typing: Type::integer()
+                    typing: Type::float()
                 },
             }
         }
