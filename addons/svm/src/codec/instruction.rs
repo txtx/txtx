d9a337820737033981e3e5c5af5fcec669ca9969
MicaiahReid <micaiahreid@gmail.com>
2025-01-09 14:50:52 -0500

    chore(svm): change `parse_instructions_map` fn to return Diag instead of String

--- addons/svm/src/codec/instruction.rs
+++ addons/svm/src/codec/instruction.rs
@@ -1,46 +1,47 @@
 use solana_sdk::instruction::{AccountMeta, Instruction};
-use txtx_addon_kit::types::stores::ValueStore;
+use txtx_addon_kit::types::{diagnostics::Diagnostic, stores::ValueStore};
 
 use crate::{
     constants::{ACCOUNT, DATA, INSTRUCTION, IS_SIGNER, IS_WRITABLE, PROGRAM_ID, PUBLIC_KEY},
     typing::SvmValue,
 };
 
-pub fn parse_instructions_map(values: &ValueStore) -> Result<Vec<Instruction>, String> {
+pub fn parse_instructions_map(values: &ValueStore) -> Result<Vec<Instruction>, Diagnostic> {
     let mut instructions = vec![];
     let instructions_data = values
         .get_expected_map(INSTRUCTION)
-        .map_err(|diag| diag.message)?
+        .map_err(|diag| diag)?
         .iter()
-        .map(|i| i.as_object().ok_or("'instruction' must be a map type".to_string()))
+        .map(|i| i.as_object().ok_or(diagnosed_error!("'instruction' must be a map type")))
         .collect::<Result<Vec<_>, _>>()?;
 
     for instruction_data in instructions_data.iter() {
         let program_id = instruction_data
             .get(PROGRAM_ID)
             .map(|p| SvmValue::to_pubkey(p))
-            .ok_or("'program_id' is required for each instruction".to_string())?
-            .map_err(|e| format!("invalid 'program_id' for instruction: {e}"))?;
+            .ok_or(diagnosed_error!("'program_id' is required for each instruction"))?
+            .map_err(|e| diagnosed_error!("invalid 'program_id' for instruction: {e}"))?;
 
         let accounts = instruction_data
             .get(ACCOUNT)
-            .ok_or("'account' is required for each instruction".to_string())?
+            .ok_or(diagnosed_error!("'account' is required for each instruction"))?
             .as_map()
-            .ok_or("'account' field for an instruction must be a map".to_string())?
+            .ok_or(diagnosed_error!("'account' field for an instruction must be a map"))?
             .iter()
-            .map::<Result<AccountMeta, String>, _>(|a| {
-                let account = a
-                    .as_object()
-                    .ok_or("each map entry of 'account' field must be an object".to_string())?;
+            .map::<Result<AccountMeta, Diagnostic>, _>(|a| {
+                let account = a.as_object().ok_or(diagnosed_error!(
+                    "each map entry of 'account' field must be an object"
+                ))?;
 
                 let pubkey = account
                     .get(PUBLIC_KEY)
                     .map(|p| SvmValue::to_pubkey(p))
-                    .ok_or(
+                    .ok_or(diagnosed_error!(
                         "each map entry of 'account' field must have a 'public_key' field"
-                            .to_string(),
-                    )?
-                    .map_err(|e| format!("invalid 'public_key' for 'account' field: {e}"))?;
+                    ))?
+                    .map_err(|e| {
+                        diagnosed_error!("invalid 'public_key' for 'account' field: {e}")
+                    })?;
 
                 let is_signer = account.get(IS_SIGNER).and_then(|b| b.as_bool()).unwrap_or(false);
                 let is_writable =
@@ -53,7 +54,7 @@ pub fn parse_instructions_map(values: &ValueStore) -> Result<Vec<Instruction>, S
             .get(DATA)
             .map(|d| {
                 d.expect_buffer_bytes_result()
-                    .map_err(|e| format!("invalid 'data' for instruction: {e}"))
+                    .map_err(|e| diagnosed_error!("invalid 'data' for instruction: {e}"))
             })
             .transpose()?
             .unwrap_or_default();
