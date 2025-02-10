6c9b9afc6ae6eb4f31ce5823aaec9691960c8e13
Ludo Galabru <ludo@txtx.builders>
2025-02-10 13:28:06 -0500

    chore: undo local patches

--- crates/txtx-core/build.rs
+++ crates/txtx-core/build.rs
@@ -1,20 +1,20 @@
 fn main() {
     // evm contract builds
     {
-        // use std::process::Command;
-        // let contracts_dir = "../../addons/evm/src/contracts";
+        use std::process::Command;
+        let contracts_dir = "../../addons/evm/src/contracts";
 
-        // println!("cargo:rerun-if-changed=build.rs");
-        // println!("cargo:rerun-if-changed={}", contracts_dir);
+        println!("cargo:rerun-if-changed=build.rs");
+        println!("cargo:rerun-if-changed={}", contracts_dir);
 
-        // println!("cargo:warning=Build script evm contracts dir: {:?}", contracts_dir);
+        println!("cargo:warning=Build script evm contracts dir: {:?}", contracts_dir);
 
-        // let exit_status = Command::new("forge")
-        //     .args(&["build"])
-        //     .current_dir(contracts_dir)
-        //     .status()
-        //     .expect("Failed to run `forge build`. Ensure Foundry is installed.");
-        // println!("cargo:warning=EVM contract build script completed: {:?}", exit_status);
-        // println!("cargo:info={}", exit_status);
+        let exit_status = Command::new("forge")
+            .args(&["build"])
+            .current_dir(contracts_dir)
+            .status()
+            .expect("Failed to run `forge build`. Ensure Foundry is installed.");
+        println!("cargo:warning=EVM contract build script completed: {:?}", exit_status);
+        println!("cargo:info={}", exit_status);
     }
 }
