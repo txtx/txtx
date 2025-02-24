b95199d7620319e2e8c2ec8d3021c9b056c4643c
MicaiahReid <micaiahreid@gmail.com>
2025-02-24 11:30:36 -0500

    fix(svm): update svm templates

--- addons/svm/src/templates/mod.rs
+++ addons/svm/src/templates/mod.rs
@@ -8,25 +8,30 @@ pub fn get_interpolated_header_template(title: &str) -> String {
     );
 }
 
-pub fn get_interpolated_addon_template(rpc_url: &str) -> String {
+pub fn get_interpolated_addon_template(rpc_url: &str, network_id: &str) -> String {
     return format!(
         r#"
 addon "svm" {{
     rpc_api_url = {}
+    network_id = {}
 }}
 "#,
-        rpc_url
+        rpc_url, network_id
     );
 }
 
 pub fn get_interpolated_signer_template(keypair_path: &str) -> String {
     return format!(
         r#"
-signer "deployer" "svm::secret_key" {{
+signer "payer" "svm::secret_key" {{
+    keypair_json = {}
+}}
+    
+signer "authority" "svm::secret_key" {{
     keypair_json = {}
 }}
 "#,
-        keypair_path
+        keypair_path, keypair_path
     );
 }
 
@@ -35,11 +40,11 @@ pub fn get_interpolated_mainnet_signer_template(_keypair_path: &str) -> String {
         r#"
 # For mainnet deployment, use web wallets, hardware wallets, or multisig for key security.
 
-# signer "deployer" "svm::web_wallet" {{
+# signer "payer" "svm::web_wallet" {{
 #   address = "YOUR_WEB_WALLET_PUBLIC_KEY"
 # }}
 
-# signer "deployer" "svm::squads" {{
+# signer "authority" "svm::squads" {{
 #   address = "YOUR_SQUAD_PUBLIC_KEY"
 # }}
 "#
@@ -52,8 +57,8 @@ pub fn get_interpolated_anchor_program_deployment_template(program_name: &str) -
 action "deploy_{}" "svm::deploy_program" {{
     description = "Deploy {} program"
     program = svm::get_program_from_anchor_project("{}") 
-    authority = signer.deployer
-    payer = signer.deployer
+    authority = signer.authority
+    payer = signer.payer
 }}
 "#,
         program_name, program_name, program_name
