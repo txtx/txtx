a3d10b2c640d1cfd40c2bf7a154ea7c0a252424f
MicaiahReid <micaiahreid@gmail.com>
2025-04-14 21:17:36 -0400

    chore(svm): remove `svm::account` fn

--- addons/svm/core/src/functions.rs
+++ addons/svm/core/src/functions.rs
@@ -72,59 +72,6 @@ lazy_static! {
             }
         },
         define_function! {
-            CreateAccountMeta => {
-                name: "account",
-                documentation: "`svm::account` encodes a public key in to an account meta object for a program instruction call.",
-                example: indoc! {r#"
-                    output "account" { 
-                        value = svm::account("3z9vL1zjN6qyAFHhHQdWYRTFAcy69pJydkZmSFBKHg1R", true, true)
-                    }
-                    // > account: { public_key: 3z9vL1zjN6qyAFHhHQdWYRTFAcy69pJydkZmSFBKHg1R, is_signer: true, is_writable: true } 
-                "#},
-                inputs: [
-                    public_key: {
-                        documentation: "The on-chain address of an account.",
-                        typing: vec![Type::string()]
-                    },
-                    is_signer: {
-                        documentation: "Specify if the account is required as a signer on the transaction.",
-                        typing: vec![Type::bool()]
-                    },
-                    is_writable: {
-                        documentation: "Specify if the account data will be modified.",
-                        typing: vec![Type::bool()]
-                    }
-                ],
-                output: {
-                    documentation: "The account meta object.",
-                    typing: Type::object(vec![ObjectProperty {
-                        name: "public_key".into(),
-                        documentation: "The public key of the account.".into(),
-                        typing: Type::string(),
-                        optional: false,
-                        tainting: false,
-                        internal: false
-                    },
-                    ObjectProperty {
-                        name: "is_signer".into(),
-                        documentation: "Specifies if the account is a signer on the instruction.".into(),
-                        typing: Type::string(),
-                        optional: false,
-                        tainting: false,
-                        internal: false
-                    },
-                    ObjectProperty {
-                        name: "is_writable".into(),
-                        documentation: "Specifies if the account is written to by the instruction.".into(),
-                        typing: Type::string(),
-                        optional: false,
-                        tainting: false,
-                        internal: false
-                    }])
-                },
-            }
-        },
-        define_function! {
             GetInstructionDataFromIdlPath => {
                 name: "get_instruction_data_from_idl_path",
                 documentation: "`svm::get_instruction_data_from_idl_path` creates encoded instruction data for a program invocation, providing type checking and serialization based on the provided IDL file.",
@@ -442,35 +389,6 @@ impl FunctionImplementation for DefaultPubkey {
         Ok(SvmValue::pubkey(Pubkey::default().to_bytes().to_vec()))
     }
 }
-
-pub struct CreateAccountMeta;
-impl FunctionImplementation for CreateAccountMeta {
-    fn check_instantiability(
-        _fn_spec: &FunctionSpecification,
-        _auth_ctx: &AuthorizationContext,
-        _args: &Vec<Type>,
-    ) -> Result<Type, Diagnostic> {
-        unimplemented!()
-    }
-
-    fn run(
-        fn_spec: &FunctionSpecification,
-        _auth_ctx: &AuthorizationContext,
-        args: &Vec<Value>,
-    ) -> Result<Value, Diagnostic> {
-        arg_checker(fn_spec, args)?;
-        let public_key = args.get(0).unwrap();
-        let is_signer = args.get(1).unwrap();
-        let is_writable = args.get(2).unwrap();
-
-        Ok(Value::object(indexmap! {
-            "public_key".to_string() => public_key.clone(),
-            "is_signer".to_string() => is_signer.clone(),
-            "is_writable".to_string() => is_writable.clone()
-        }))
-    }
-}
-
 pub struct GetInstructionDataFromIdl;
 impl FunctionImplementation for GetInstructionDataFromIdl {
     fn check_instantiability(
