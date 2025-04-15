0cc0a3779e9b9b34ad341fa0300454d9c9948c07
MicaiahReid <micaiahreid@gmail.com>
2025-04-14 21:07:46 -0400

    feat(kit/core): update Type::object/map to differentiate arbitrary/strict type def

--- crates/txtx-addon-kit/src/types/mod.rs
+++ crates/txtx-addon-kit/src/types/mod.rs
@@ -7,7 +7,7 @@ use hcl_edit::structure::Block;
 use serde::de::Error;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use sha2::{Digest, Sha256};
-use types::{ObjectProperty, Type};
+use types::{ObjectDefinition, ObjectProperty, Type};
 
 use crate::helpers::fs::FileLocation;
 
@@ -349,7 +349,7 @@ pub trait EvaluatableInput {
     fn optional(&self) -> bool;
     fn typing(&self) -> &Type;
     fn name(&self) -> String;
-    fn as_object(&self) -> Option<&Vec<ObjectProperty>> {
+    fn as_object(&self) -> Option<&ObjectDefinition> {
         self.typing().as_object()
     }
     fn as_array(&self) -> Option<&Box<Type>> {
@@ -358,7 +358,7 @@ pub trait EvaluatableInput {
     fn as_action(&self) -> Option<&String> {
         self.typing().as_action()
     }
-    fn as_map(&self) -> Option<&Vec<ObjectProperty>> {
+    fn as_map(&self) -> Option<&ObjectDefinition> {
         self.typing().as_map()
     }
 }
