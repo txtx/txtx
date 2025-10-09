bef2e563aca27249c020aff5edc1188948a69188
MicaiahReid <micaiahreid@gmail.com>
2025-10-09 13:16:53 -0400

    introduce in-memory scaffold

--- addons/svm/core/src/templates/mod.rs
+++ addons/svm/core/src/templates/mod.rs
@@ -87,7 +87,24 @@ action "deploy_{}" "svm::deploy_program" {{
     program = svm::get_program_from_anchor_project("{}") 
     authority = signer.authority
     payer = signer.payer
-    // When targeting a Surfnet, write directly to the program account via cheatcode for instantaneous deployments
+    // Optional: if you want to deploy the program via a cheatcode when targeting a Surfnet, set `instant_surfnet_deployment = true`
+    // Deploying via a cheatcode will write the program data directly to the program account, rather than sending transactions.
+    // This will make deployments instantaneous, but is deviating from how the deployments will take place on devnet/mainnet.
+    // instant_surfnet_deployment = true
+}}
+"#,
+        program_name, program_name, program_name
+    );
+}
+
+pub fn get_in_memory_interpolated_anchor_program_deployment_template(program_name: &str) -> String {
+    return format!(
+        r#"
+action "deploy_{}" "svm::deploy_program" {{
+    description = "Deploy {} program"
+    program = svm::get_program_from_anchor_project("{}") 
+    authority = signer.authority
+    payer = signer.payer
     instant_surfnet_deployment = true
 }}
 "#,
@@ -103,7 +120,24 @@ action "deploy_{}" "svm::deploy_program" {{
     program = svm::get_program_from_native_project("{}") 
     authority = signer.authority
     payer = signer.payer
-    // When targeting a Surfnet, write directly to the program account via cheatcode for instantaneous deployments
+    // Optional: if you want to deploy the program via a cheatcode when targeting a Surfnet, set `instant_surfnet_deployment = true`
+    // Deploying via a cheatcode will write the program data directly to the program account, rather than sending transactions.
+    // This will make deployments instantaneous, but is deviating from how the deployments will take place on devnet/mainnet.
+    // instant_surfnet_deployment = true
+}}
+"#,
+        program_name, program_name, program_name
+    );
+}
+
+pub fn get_in_memory_interpolated_native_program_deployment_template(program_name: &str) -> String {
+    return format!(
+        r#"
+action "deploy_{}" "svm::deploy_program" {{
+    description = "Deploy {} program"
+    program = svm::get_program_from_native_project("{}") 
+    authority = signer.authority
+    payer = signer.payer
     instant_surfnet_deployment = true
 }}
 "#,
